a11b4246a393c7001727565aaa1e0fe3ca2c981e
LierNj <150467772+cameramanpov@users.noreply.github.com>
2024-09-06 22:14:53 +0200

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,4 @@
+// Masquer le loader et afficher le contenu de la page une fois chargée
+window.addEventListener('load', function () {
+    document.getElementById('loader').style.display = 'none';  // Cacher le loader
+});
